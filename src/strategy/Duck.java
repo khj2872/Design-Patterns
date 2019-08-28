a25932b14d25eb7d636382be206f1d386abb2f88
khj2872 <khj2872@daum.net>
2019-08-29 00:19:42 +0900

    스트레티지 패턴

---
+++ src/strategy/Duck.java
@@ -0,0 +1,31 @@
+package strategy;
+
+public abstract class Duck {
+
+    QuackBehavior quackBehavior;
+    FlyBehavior flyBehavior;
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void swim() {
+        System.out.println("모든 오리들은 수영이 가능");
+    }
+
+    abstract void display();
+
+
+}
